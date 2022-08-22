64de7c0296543866bab7707b338937fcadb609fc
astrofx123 <96091059+astrofx123@users.noreply.github.com>
2022-08-22 07:12:25 -0400

    .

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // Enter here your connection string
-mongoose.connect('mongodb+srv://admin:admin@cluster0.vmkju.mongodb.net/DriverDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://dbuser:dbuser@cluster0.fzasn.mongodb.net/?retryWrites=true&w=majority');
 
 mongoose.connection.on("connected", function(){
     console.log("Application is connected to Database");
