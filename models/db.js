40d3d42c45dcd2eb1de52319dd6b61f9add03be5
astrofx123 <96091059+astrofx123@users.noreply.github.com>
2022-08-22 07:14:20 -0400

    .

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // Enter here your connection string
-mongoose.connect('mongodb+srv://dbuser:dbuser@cluster0.fzasn.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://admin:V2hPpaIL13hCsNa2@cluster0.tzrwn4j.mongodb.net/?retryWrites=true&w=majorit');
 
 mongoose.connection.on("connected", function(){
     console.log("Application is connected to Database");
